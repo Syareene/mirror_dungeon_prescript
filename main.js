8d5d80b355896e31722c3243f9b847b738666f76
syareene <soramame3468@icloud.com>
2026-04-03 18:01:03 +0900

    全パックデータ導入完

--- main.js
+++ main.js
@@ -3,12 +3,14 @@
 
 // これisactive保存しちゃってるけどセッションとかクッキーに保存して取得する形になるかもねぇ
 
-const sourceData = [
+console.log("JSファイルが読み込まれました"); // 読み込み確認ログ
+
+const packData = [
     { text: "忘れられた者たち", locations: [1], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/a.jpg" },
     { text: "属せない", locations: [1], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
     { text: "カジノプアー", locations: [1], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
-    { text: "工場自動化", locations: [1, 2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
-    { text: "愛することのできない", locations: [1, 2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "工場自動化", locations: [1,2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "愛することのできない", locations: [1,2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
     { text: "釘と金槌", locations: [1], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
     { text: "信仰と侵蝕", locations: [1], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
     { text: "向き合わない", locations: [2,3], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
@@ -31,11 +33,81 @@ const sourceData = [
     { text: "教本", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
     { text: "剣と作品", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
     { text: "断ち切れない", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
-    { text: "テキストC", locations: [], appear6to10: true, appear11to15: true, isActive: true, imagePath: "path/to/c.jpg" },
-    { text: "テキストD", locations: [], appear6to10: false, appear11to15: true, isActive: true, imagePath: "path/to/d.jpg" },
-    { text: "テキストE", locations: [], appear6to10: true, appear11to15: true, isActive: false, imagePath: "path/to/e.jpg" }, // isActiveがfalseなので選ばれない
-    { text: "テキストF", locations: [], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/f.jpg" },
-    { text: "テキストG", locations: [], appear6to10: false, appear11to15: true, isActive: true, imagePath: "path/to/g.jpg" }
+    { text: "ヘルズチキン", locations: [2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "う・み・ダ", locations: [2,3], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "20番区の奇跡", locations: [3,4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "肉切骨断", locations: [3,4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "時間殺人時間", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "ワープ特急殺人事件", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "紫の白昼", locations: [3,4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "鏡屈折鉄道1号線 -MADNESS-", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "鏡屈折鉄道2号線 -MÖBIUS", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "鏡屈折鉄道3号線 -MirrorclockOrangeroad-", locations: [5], appear6to10: true, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "鏡屈折鉄道4号線 -Masquerade- 第3区間", locations: [5], appear6to10: true, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "鏡屈折鉄道4号線 -Masquerade- 第4区間", locations: [5], appear6to10: true, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "20番区の奇跡 BokGak", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "弾丸が撃った終止符", locations: [3,4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "LCB定期診断", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "肉切骨断 BokGak", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "深夜清掃", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "鏡屈折鉄道5号線 -MerryGoRound-", locations: [5], appear6to10: true, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "憎悪と絶望", locations: [3,4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "時間殺人時間 BokGak", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "切磋琢春", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "ワープ特急殺人事件 BokGak", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "琥珀の夕暮", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "LCB定期診断 BokGak", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "切り裂く者たち", locations: [4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "斬るべきもの", locations: [1,2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "刺し貫く者たち", locations: [4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "貫くべきもの", locations: [1,2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "壊し砕く者たち", locations: [4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "壊れるもの", locations: [1,2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "抑圧された怒り", locations: [3,4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "解放された怒り", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "感情に押し潰されるもの", locations: [2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "のめり込む色欲", locations: [3,4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "縛り付ける色欲", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "感情に惑わされるもの", locations: [2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "空回りする怠惰", locations: [3,4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "止まってしまった怠惰", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "感情の前に怠けたもの", locations: [2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "食い尽くした暴食", locations: [3,4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "溢れかえった暴食", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "感情に飢えたもの", locations: [2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "戦略する憂鬱", locations: [3,4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "沈んだ憂鬱", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "感情に沈水したもの", locations: [2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "虚勢の傲慢", locations: [3,4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "独善的な傲慢", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "感情に服従したもの", locations: [2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "見窄らしい嫉妬", locations: [3,4], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "哀れな嫉妬", locations: [5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "感情で評価されたもの", locations: [2], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "燃え上がる揺らめき", locations: [3], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "火旺の節", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "漏れ出した赤血", locations: [3], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "屍山血海", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "ぐらつく波動", locations: [3], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "異常震域", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "壊す外力", locations: [3], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "破竹の勢い", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "沈む疼き", locations: [3], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "沈潜殺到", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "吐かれた一息", locations: [3], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "循環呼吸", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "満ちゆく動力", locations: [3], appear6to10: false, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "霹靂閃電", locations: [4,5], appear6to10: true, appear11to15: false, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "N社 新九人会", locations: [], appear6to10: false, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "開花する緑林", locations: [], appear6to10: false, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "3号線 - 終着駅", locations: [], appear6to10: false, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "永劫のくびき", locations: [], appear6to10: false, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "う・み・ダ・かに・バル", locations: [], appear6to10: false, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "終わりなき袋小路", locations: [], appear6to10: false, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "血鬼になれなかった者たち", locations: [], appear6to10: false, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "美しい声", locations: [], appear6to10: false, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "緑の黎明", locations: [], appear6to10: false, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
+    { text: "とある図書館の、ある本の中へ", locations: [], appear6to10: false, appear11to15: true, isActive: true, imagePath: "path/to/b.jpg" },
 ];
 
 // 2. ボタンクリック時のイベントリスナー
@@ -52,7 +124,7 @@ document.getElementById('generateBtn').addEventListener('click', () => {
     }
 
     // 抽選用のプールを作成（isActiveがtrueのものだけをコピー）
-    let availablePool = sourceData.filter(item => item.isActive);
+    let availablePool = packData.filter(item => item.isActive);
     const results = [];
 
     // パースされた数字ごとに抽選処理を実行
